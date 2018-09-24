79a71f4bd72333f3e41f04898ed600e64a527e91
Naeem Ahmed <bdnaeem3@gmail.com>
2018-09-24 13:05:13 +0600

    User Profile Image Created and added signals when creating new user

--- learning_django/urls.py
+++ learning_django/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_view
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('superuser/', admin.site.urls),
@@ -26,3 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_view.profile, name='user-profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
