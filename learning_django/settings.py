c593f9470cf406cd3c5bd9a09aecbd1a219a683e
Naeem Ahmed <bdnaeem3@gmail.com>
2018-10-03 15:18:00 +0600

    Added password reset email facility

--- learning_django/settings.py
+++ learning_django/settings.py
@@ -131,3 +131,10 @@ LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('MAIL_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('MAIL_PASSWORD')
