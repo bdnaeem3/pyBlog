229c4f3b89a4ba752e1a24e87b4b03bcb00ee133
Naeem Ahmed <bdnaeem3@gmail.com>
2018-10-03 14:39:54 +0600

    User posts page created

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('post/create', views.CreatePost.as_view(), name='create-post'),
     path('post/<int:pk>/update', views.UpdatePost.as_view(), name='update-post'),
     path('post/<int:pk>/delete', views.DeletePost.as_view(), name='delete-post'),
+    path('user/<str:username>', views.UserPost.as_view(), name='user-post'),
     path('about', views.about, name='blog-about'),
 ]
